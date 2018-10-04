8fdfbc7ca742d34b3157b55a866531127c700838
Anoop <anoop@meemmemory.com>
2018-10-04 17:03:49 +0530

    TouchIDAuthetication initial commit

--- SwiftTouchAuthentication/ViewController.swift
+++ SwiftTouchAuthentication/ViewController.swift
@@ -7,14 +7,65 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.authenticateUserUsingTouchId()
     }
-
-
+    
+    fileprivate func authenticateUserUsingTouchId() {
+        print("Authentication started")
+        let context = LAContext()
+        if context.canEvaluatePolicy(LAPolicy.deviceOwnerAuthentication, error: nil) {
+            self.evaulateTocuhIdAuthenticity(context: context)
+        }
+    }
+    
+    func evaulateTocuhIdAuthenticity(context: LAContext) {
+//        guard let lastAccessedUserName = UserDefaults.standard.object(forKey: "lastAccessedUserName") as? String else { return }
+        let lastAccessedUserName = "anoop@meemmemory.com"
+        context.evaluatePolicy(LAPolicy.deviceOwnerAuthentication, localizedReason: lastAccessedUserName) { (authSuccessful, authError) in
+            if authSuccessful {
+                print("Authentication is successful, proceed your work")
+            } else {
+                if let error = authError as? LAError {
+                    self.showError(error: error)
+                }
+            }
+        }
+    }
+    
+    func showError(error: LAError) {
+        var message: String = ""
+        switch error.code {
+        case LAError.authenticationFailed:
+            message = "Authentication was not successful because the user failed to provide valid credentials. Please enter password to login."
+            break
+        case LAError.userCancel:
+            message = "Authentication was canceled by the user"
+            break
+        case LAError.userFallback:
+            message = "Authentication was canceled because the user tapped the fallback button"
+            break
+        case LAError.biometryNotEnrolled:
+            message = "Authentication could not start because Touch ID has no enrolled fingers."
+            break
+        case LAError.passcodeNotSet:
+            message = "Passcode is not set on the device."
+            break
+        case LAError.systemCancel:
+            message = "Authentication was canceled by system"
+            break
+        default:
+            message = error.localizedDescription
+            break
+        }
+        print(message)
+    }
+    
 }
 
